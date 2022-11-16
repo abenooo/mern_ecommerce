43c8563f609765e2ebacc201402bd53f0a5cfae3
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-11-16 22:34:51 +0300

    Add database configeration

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+const connectDb = async () =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+            // useCreateIndex: true
+        })
+        console.log(`Mongo connected: ${conn.connection.host}`)
+    }
+    
+    catch (error) {
+        console.error(`Error,${error.message}`)
+        process.exit(1)
+    }
+
+}
+export default connectDb
