f499b597ce646313ddb665d8a7985d6a4139ff92
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-11-16 15:05:00 +0300

    Adding container

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,35 @@
 import React from "react"
-import products from "../products"
-import { Row, Col } from "react-bootstrap"
+import { useEffect, useState } from "react"
+// import products from "../products"
+import axios from "axios"
+import { Row, Col, Container } from "react-bootstrap"
 import Product from "../componets/Product"
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    // console.log("hello from useEffect")
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products")
+      setProducts(data)
+      console.log(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
-      <h1>Latest products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+      <main>
+        <Container>
+          <h1>Latest products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                <h3>{product.name}</h3>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
     </>
   )
 }
