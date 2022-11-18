6e019fd9c7c85f5be2ba96a1ee9a7841a959a901
aR <abenezerkifle000@gmail.com>
2022-11-18 12:09:44 +0300

    Adding route

--- backend/test.js
+++ backend/test.js
@@ -1,22 +1,17 @@
 import express from "express"
-import products from "./data/products.js"
 import connectDb from "./config/db.js"
 import dotenv from "dotenv"
 import colors from 'colors'
-const app = express()
-    // adding dotenv
+import productRoutes from './routes/productRoutes.js'
+// adding dotenv
 dotenv.config()
     // connection string
 connectDb()
+const app = express()
+app.use('/api/products', productRoutes)
+
 const Port = process.env.Port || 5000
-app.listen(Port, console.log(`server is running in ${process.env.NODE_ENV} mode on the port ${Port}`.yellow.bold))
-app.get("/", (req, res) => {
-    res.send("Backend running on nodemon ....., no refresh using nodemon")
-})
-app.get("/api/products", (req, res) => {
-    res.json(products)
-})
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.listen(
+    Port,
+    console.log(`server is running in ${process.env.NODE_ENV} mode on the port ${Port}`.yellow.bold)
+)
