049115f3230b69afe98d655307423a9bb2538e5c
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-11-21 11:13:34 +0300

    Adding custom error middle for the developemnt

--- backend/test.js
+++ backend/test.js
@@ -1,17 +1,29 @@
 import express from "express"
 import connectDb from "./config/db.js"
 import dotenv from "dotenv"
-import colors from 'colors'
-import productRoutes from './routes/productRoutes.js'
+import colors from "colors"
+import productRoutes from "./routes/productRoutes.js"
 // adding dotenv
 dotenv.config()
-    // connection string
+// connection string
 connectDb()
 const app = express()
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes)
+// create middleware fro error message
+app.use((err, req, req, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  })
+})
 
 const Port = process.env.Port || 5000
 app.listen(
-    Port,
-    console.log(`server is running in ${process.env.NODE_ENV} mode on the port ${Port}`.yellow.bold)
+  Port,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on the port ${Port}`
+      .yellow.bold
+  )
 )
