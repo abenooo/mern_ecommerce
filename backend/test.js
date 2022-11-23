cb4f936f419c8ca6d8d14b7a93e35fb0854f542c
aR <abenezerkifle000@gmail.com>
2022-11-23 11:01:33 +0300

    Adding middleware

--- backend/test.js
+++ backend/test.js
@@ -3,27 +3,25 @@ import connectDb from "./config/db.js"
 import dotenv from "dotenv"
 import colors from "colors"
 import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware"
 // adding dotenv
 dotenv.config()
-// connection string
+    // connection string
 connectDb()
 const app = express()
 app.use("/api/products", productRoutes)
+
+// 404 
+app.use(notFound)
+
 // create middleware fro error message
-app.use((err, req, req, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  })
-})
+app.use(errorHandler)
 
 const Port = process.env.Port || 5000
 app.listen(
-  Port,
-  console.log(
-    `server is running in ${process.env.NODE_ENV} mode on the port ${Port}`
-      .yellow.bold
-  )
+    Port,
+    console.log(
+        `server is running in ${process.env.NODE_ENV} mode on the port ${Port}`
+        .yellow.bold
+    )
 )
