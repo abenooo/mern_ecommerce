8f5045ed0cecaf28b8ddbbfea12455cfda3b53c0
aR <abenezerkifle000@gmail.com>
2022-11-23 15:14:27 +0300

    Remove usestat and use reduc hook for product state

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,27 +1,24 @@
-import React from "react"
-import { useEffect, useState } from "react"
-// import products from "../products"
-import axios from "axios"
-import { Row, Col, Container } from "react-bootstrap"
-import Product from "../componets/Product"
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { Row, Col, Container } from "react-bootstrap";
+import Product from "../componets/Product";
+import { listProducts } from "../actions/productActions";
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch();
   useEffect(() => {
-    // console.log("hello from useEffect")
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products")
-      setProducts(data)
-      console.log(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  const products = [];
   return (
     <>
       <main>
         <Container>
           <h1>Latest products</h1>
           <Row>
-            {products.map((product,index) => (
+            {products.map((product, index) => (
               <Col key={index} sm={12} md={6} lg={4} xl={3}>
                 <h3>{product.name}</h3>
                 <Product product={product} />
@@ -31,7 +28,7 @@ const HomeScreen = () => {
         </Container>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
