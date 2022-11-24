e5a1bdf3d2f143174632ec5e4808496ba61cbbf9
aR <abenezerkifle000@gmail.com>
2022-11-24 10:20:37 +0300

    Fetch data using redux and remove state

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 import { Row, Col, Container } from "react-bootstrap";
 import Product from "../componets/Product";
 import { listProducts } from "../actions/productActions";
 const HomeScreen = () => {
   const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  const products = [];
   return (
     <>
       <main>
         <Container>
           <h1>Latest products</h1>
-          <Row>
-            {products.map((product, index) => (
-              <Col key={index} sm={12} md={6} lg={4} xl={3}>
-                <h3>{product.name}</h3>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
+          {loading ? (
+            <h2>loading</h2>
+          ) : error ? (
+            <h3>{error}</h3>
+          ) : (
+            <Row>
+              {products.map((product, index) => (
+                <Col key={index} sm={12} md={6} lg={4} xl={3}>
+                  <h3>{product.name}</h3>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )}
         </Container>
       </main>
     </>
